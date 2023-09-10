5f9760d9924b6917d80458d4173331a6a97d6c5c
ExpertNugget <deaconjf0427@outlook.com>
2023-09-10 08:27:25 -0600

    fixed account creation

--- src/account/views.py
+++ src/account/views.py
@@ -10,7 +10,7 @@ def registration_view(request):
         if form.is_valid():
             form.save()
             email = form.cleaned_data.get('email')
-            raw_password = form.changed_data.get('password1')
+            raw_password = form.cleaned_data.get('password1')
             account = authenticate(email=email, password=raw_password)
             login(request, account)
             return redirect('home')
